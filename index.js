0d63ef1f88c38c58fbd4acab2f4fdc974d1d9659
dci <rudeshdhote@gmail.com>
2022-02-05 23:53:16 +0100

    fix input element

--- index.js
+++ index.js
@@ -4,45 +4,25 @@ const addToFunc = () => {
   // gets value from main text area
   let input = document.getElementById("inputStatus").value;
   // creates element for the value of input
-  let title = document.createElement("h6");
+
+  let title = document.getElementById("title");
+  let date = document.getElementById("date");
+
   let status = document.createElement("p");
-  let reply = document.createElement("ul");
-
-  let replyList = document.createElement("li");
-
+  let d = new Date();
+  title.innerHTML = "User :";
+  date.innerHTML = `${d.getHours()}:${d.getMinutes()} `;
   //creates a node for the original input
   let textnode = document.createTextNode(input);
   status.appendChild(textnode);
+  status.prepend(date);
   status.prepend(title);
-  status.appendChild(reply);
 
-  reply.appendChild(replyList);
   document.getElementById("addTask").appendChild(status);
   status.classList.add("statusBox");
-  replyList.classList.add("reply");
-
-  //creates a container for the remove button
-  let statusBox = document.createElement("div");
-  //creates a button to delete status
-  let removeTask = document.createElement("input");
-  // adds type of which it is a button
-  removeTask.setAttribute("type", "button");
-  removeTask.classList.add("removeButton");
-  // sets the buttons value
-  // sets the button next to the status text
-  //sets the button to remove status
-  removeTask.addEventListener(
-    "click",
-    function () {
-      status.parentNode.removeChild(status);
-    },
-    false
-  );
-
-  //For reply
 };
 
 const eraseText = () => {
-  //erases placeholder text in textarea
+  //erases placeholder text
   document.getElementById("inputStatus").value = "";
 };
